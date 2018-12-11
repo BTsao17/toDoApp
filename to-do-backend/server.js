8e3ca4dcc57c8fee4eb2c9e71c3f5b2f35df0f08
Bonnie Tsao <IceJade1788@gmail.com>
2018-12-11 02:40:03 -0800

    removed initial tester code for fetching and adding items in database.  Commented out code for fetching data by category id

--- to-do-backend/server.js
+++ to-do-backend/server.js
@@ -12,69 +12,39 @@ app.use('/toDos', toDoRouter)
 
 //testing
 const Model = require('./models/models')
-//create new row in ToDos - successful and changed my original toDopost controller.
-
-// const newToDo = new Model.ToDo({
-//     task: 'Vacuum the carpet',
-//     completed: false,
-//     category_id: 1
-// })
-// newToDo.save()
-// .then(toDo => {
-//     console.log(toDo.attributes)
-// })
-
-//fetch all Categories
-// Model.Category
-//     .fetchAll()
-//     .then(categories => {
-//         //console.log(categories.models)
-//         console.log(categories.models.map(category => category.attributes))
-//     })
 
 app.get('/category', (request, response) => {
     Model.Category
         .fetchAll()
         .then(categories => {
             const data = categories.models.map(category => category.attributes)
-            //console.log(data)
             response.json(data)
         })
         .catch(err => console.log(err))
 })
 
-//fetch based on category ID
-// Model.ToDo
-//     .where('category_id', 1)
-//     .fetchAll()
-//     .then(toDos => {
-//         console.log(toDos.models.map(toDo => toDo.attributes))
-//     })
-
-app.get('/:categoryID', (request, response) => {
-    //console.log(request.params)
-    const { categoryID } = request.params //data type is a string
-    //console.log(typeof categoryID)
-
-    if (Number(categoryID) === 0) {
-        Model.ToDo
-        .fetchAll()
-        .then(toDos => {
-            const data = toDos.models.map(toDo => toDo.attributes)
-            response.json(data)
-        })
-        .catch(err => console.log(err))
-    } else {
-        Model.ToDo
-            .where('category_id', Number(categoryID))
-            .fetchAll()
-            .then(toDos => {
-                const data = toDos.models.map(toDo => toDo.attributes)
-                //console.log(data)
-                response.json(data)
-            })
-    }
-})
+//this endpoint no longer necessary as I've changed how we can view to-dos by categories
+// app.get('/:categoryID', (request, response) => {
+//     const { categoryID } = request.params 
+//     if (Number(categoryID) === 0) {
+//         Model.ToDo
+//         .fetchAll()
+//         .then(toDos => {
+//             const data = toDos.models.map(toDo => toDo.attributes)
+//             response.json(data)
+//         })
+//         .catch(err => console.log(err))
+//     } else {
+//         Model.ToDo
+//             .where('category_id', Number(categoryID))
+//             .fetchAll()
+//             .then(toDos => {
+//                 const data = toDos.models.map(toDo => toDo.attributes)
+//                 //console.log(data)
+//                 response.json(data)
+//             })
+//     }
+// })
 
 app.listen(port, () => {
     console.log(`listening at ${port}`)
