4a92945adc1863ac69c21770a440efa69f699d24
Bonnie Tsao <IceJade1788@gmail.com>
2019-07-23 15:24:27 -0700

    change knexfile connection to url

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,4 @@
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URL || {
-    user: 'bonniet',
-    password: '',
-    database: 'to-do'
-  }
+  connection: process.env.DATABASE_URL || 'postgresql://localhost/to-do'
 }
