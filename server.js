bea9f35834bb453e3bb7a303bc1b022127afeb73
Bonnie Tsao <IceJade1788@gmail.com>
2019-07-23 15:31:12 -0700

    remove cors from dependency and server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const port = process.env.PORT || process.argv[2] || 8080
 
 const toDoRouter = require('./routes/toDos')
 
 app.use(bodyParser.json())
-app.use(cors())
 app.use('/toDos', toDoRouter)
 
 //testing
