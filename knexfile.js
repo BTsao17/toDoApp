970bc03768ae082d1736744462d840bcbf3ceb1b
Bonnie Tsao <IceJade1788@gmail.com>
2019-07-24 03:24:57 -0700

    remove comments knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,4 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://czmibirwipyxsd:ede20e44d66c36ffdadb5c7fab17537d7fe6baa1914568f11666e414be4bb77c@ec2-107-20-168-237.compute-1.amazonaws.com:5432/d23snv372p7j98',
   },
-
-  // client: 'pg',
-  // connection: process.env.DATABASE_URL || 'postgresql://localhost/to-do'
 }
