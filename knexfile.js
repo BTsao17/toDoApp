629abd2f5db3bbca4ad0b3af16001e9eee155018
Bonnie Tsao <IceJade1788@gmail.com>
2019-07-24 02:53:22 -0700

    production url

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection:
-      process.env.DATABASE_URL || 'postgres://eshyjhqabberlx:325ad57f91999c244318e3625685d2a49f58cc4eb9994225e4be00424edeea14@ec2-107-20-168-237.compute-1.amazonaws.com:5432/d97q96k3grcjiv',
+    connection: 'postgres://czmibirwipyxsd:ede20e44d66c36ffdadb5c7fab17537d7fe6baa1914568f11666e414be4bb77c@ec2-107-20-168-237.compute-1.amazonaws.com:5432/d23snv372p7j98',
   },
 
   // client: 'pg',
