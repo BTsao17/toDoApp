617dd6c4641364522ae3f0521b32b420109611a7
Bonnie Tsao <IceJade1788@gmail.com>
2019-08-15 12:50:11 -0700

    remove commented out Modal component

--- client/src/components/AddToDoForm.js
+++ client/src/components/AddToDoForm.js
@@ -3,7 +3,6 @@ import { AddCategoryModal } from './';
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-//import Modal from 'react-bootstrap/Modal';
 
 class AddToDoForm extends Component {
   constructor() {
@@ -82,56 +81,5 @@ class AddToDoForm extends Component {
     );
   }
 }
-
-// class AddCategoryModal extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       newCategory: '',
-//     };
-//   }
-//   handleChange = (e) => {
-//     this.setState({
-//       [e.target.id]: e.target.value,
-//     });
-//   };
-//   handleSubmit = (e) => {
-//     this.props.addCategory(this.state);
-//     this.setState({
-//       newCategory: '',
-//     });
-//     this.props.onHide();
-//   };
-//   render() {
-//     return (
-//       <Modal show={this.props.show} size="lg" centered>
-//         <Modal.Header>
-//           <Modal.Title>Custom Category</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <Form>
-//             <Form.Group>
-//               <Form.Label>Category Name:</Form.Label>
-//               <Form.Control id="newCategory" type="text" value={this.state.newCategory} onChange={this.handleChange} />
-//             </Form.Group>
-//           </Form>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={this.props.onHide}>
-//             Close
-//           </Button>
-//           <Button
-//             type="submit"
-//             variant="info"
-//             onClick={this.handleSubmit}
-//             disabled={this.state.newCategory === '' ? true : false}
-//           >
-//             Add
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-//     );
-//   }
-// }
 
 export default AddToDoForm;
