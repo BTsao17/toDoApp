61c85b98b6571cad4c9f2b587bbfe4b39605dd11
Bonnie Tsao <IceJade1788@gmail.com>
2019-08-15 13:03:29 -0700

    submit form by pressting return, clear new category text when clicking close button on modal

--- client/src/components/AddCategoryModal.js
+++ client/src/components/AddCategoryModal.js
@@ -10,34 +10,43 @@ class AddCategoryModal extends Component {
       newCategory: '',
     };
   }
+
   handleChange = (e) => {
     this.setState({
       [e.target.id]: e.target.value,
     });
   };
-  handleSubmit = (e) => {
+
+  handleSubmit = () => {
     this.props.addCategory(this.state);
     this.setState({
       newCategory: '',
     });
     this.props.onHide();
   };
+
+  handleClose = () => {
+    this.setState({
+      newCategory: '',
+    });
+    this.props.onHide();
+  };
+
   render() {
     return (
       <Modal show={this.props.show} size="lg" centered>
         <Modal.Header>
           <Modal.Title>Custom Category</Modal.Title>
         </Modal.Header>
+        <Form>
         <Modal.Body>
-          <Form>
             <Form.Group>
               <Form.Label>Category Name:</Form.Label>
               <Form.Control id="newCategory" type="text" value={this.state.newCategory} onChange={this.handleChange} />
             </Form.Group>
-          </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={this.props.onHide}>
+          <Button variant="secondary" onClick={this.handleClose}>
             Close
           </Button>
           <Button
@@ -49,6 +58,7 @@ class AddCategoryModal extends Component {
             Add
           </Button>
         </Modal.Footer>
+        </Form>
       </Modal>
     );
   }
